15e6c3b219fcebae7e0d974d0f1efabf938477f5
pbreyna <reynasandrip@gmail.com>
2024-02-20 18:54:58 +0100

    proyecto

---
+++ src/setosa.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, render_template
+from pickle import load
+import numpy as np
+import joblib as joblib
+import os 
+
+model=joblib.load('iris_model_LR.pkl')
+#model=joblib.load('/workspaces/ML-WEB-APP-USING-FLASK/src/iris_model_LR.pkl')
+#model=joblib.load('/workspaces/ML-WEB-APP-USING-FLASK/models/iris_model_LR.pkll')
+
+app=Flask(__name__)
+IMG_FOLDER=os.path.join('static','IMG')
+app.config['UPLOAD_FOLDER']=IMG_FOLDER
+@app.route('/')
+def index():
+    return render_template('index.html')
+class_dict = {
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+    "2": "Iris virginica",
+    "Iris-versicolor": "Iris versicolor",
+    "Iris-virginica": "Iris virginica"  
+}
+
+@app.route('/',methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        val1 = float(request.form['val1'])
+        val2 = float(request.form['val2'])
+        val3 = float(request.form['val3'])
+        val4 = float(request.form['val4'])
+        data = [[val1, val2, val3, val4]]
+        prediction = str(model.predict(data)[0])
+        pred_class = class_dict[prediction]
+    else:
+        pred_class = None
+    return render_template('index.html', prediction=pred_class)
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
+
+
+
+
+"""from flask import Flask, request, render_template
+from pickle import load
+import numpy as np
+import joblib as joblib
+import os 
+
+model = joblib.load('/workspaces/ML-WEB-APP-USING-FLASK/src/iris_model_LR.pkl')
+
+app=Flask(__name__)
+IMG_FOLDER=os.path.join('static','IMG')
+app.config['UPLOAD_FOLDER']=IMG_FOLDER
+@app.route('/')
+def index():
+    return render_template('index.html')
+class_dict = {
+    "0": "Iris setosa",
+    "1": "Iris versicolor",
+    "2": "Iris virginica",
+    "Iris-versicolor": "Iris versicolor",
+    "Iris-virginica": "Iris virginica"  
+}
+
+
+@app.route('/', methods=['GET','POST'])
+def home():
+    if request.method == 'POST':
+        val1 = float(request.form['val1'])
+        val2 = float(request.form['val2'])
+        val3 = float(request.form['val3'])
+        val4 = float(request.form['val4'])
+        data = [[val1, val2, val3, val4]]
+        prediction = str(model.predict(data)[0])
+        print("Prediction:", prediction)  
+        pred_class = class_dict[prediction]
+    else:
+        pred_class = None
+    return render_template('index.html', prediction=pred_class)
+if __name__ == '__main__':
+    app.run(debug=True)
+"""
